5d542b9e0a3d954c32ad3b7686ac279a8a9b551e
ADITI CHOUHAN <48049926+thisisaditichouhan@users.noreply.github.com>
2023-04-02 18:26:24 +0530

    This is pGen, code v1.

---
+++ app.js
@@ -0,0 +1,71 @@
+const upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerSet = "abcdefghijklmnopqrstuvwxyz"
+const numberSet = "1234567890"
+const symbolSet = "~!@#$%^&*()_+/"
+
+// selectors
+const passBox = document.getElementById("pass-box")
+const totalChar = document.getElementById("total-char")
+const upperInput = document.getElementById("upper-case")
+const lowerInput = document.getElementById("lower-case")
+const numberInput = document.getElementById("numbers")
+const symbolInput = document.getElementById("symbols")
+
+
+
+const getRandomData = (dataSet) => {
+    return dataSet[Math.floor(Math.random() * dataSet.length)]
+}
+
+const generatePassword = (password = "") => {
+    if (upperInput.checked) {
+        password += getRandomData(upperSet)
+    }
+    if (lowerInput.checked) {
+        password += getRandomData(lowerSet)
+    }
+    if (numberInput.checked) {
+        password += getRandomData(numberSet)
+    }
+    if (symbolInput.checked) {
+        password += getRandomData(symbolSet)
+    }
+    if (password.length < totalChar.value) {
+        return generatePassword(password)
+    }
+
+    passBox.innerText = truncateString(password, totalChar.value);
+}
+
+
+generatePassword();
+
+document.getElementById("btn").addEventListener(
+    "click",
+    function() {
+        generatePassword();
+    }
+
+)
+
+
+function truncateString(str, num) {
+    if (str.length > num) {
+        let subStr = str.substring(0, num);
+        return subStr;
+    } else {
+        return str;
+    }
+}
+
+function copyToClipboard() {
+    var passBox = document.getElementById("pass-box");
+    var range = document.createRange();
+    range.selectNode(passBox);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+    alert("Password copied to clipboard!");
+  }
+  
